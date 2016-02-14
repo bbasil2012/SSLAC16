14e4cb1e490e95876df77de925670ad1bde20f2c
bbasil <bbasil@mavericks.ifdk.lan>
2016-02-14 18:33:20 +0300

    updates

--- tools/flashme.sh
+++ tools/flashme.sh
@@ -2,4 +2,4 @@ OS=`uname`
 M=`uname -m`
 DIR=`uname`'-'`uname -m`
 echo $DIR
-$DIR/esptool -v -cd nodemcu -cp $1 -cb 115200 -ca 0x00000000 -cf ../bin/$2/0x0-SSLAC16-v0.35rev5.bin
+$DIR/esptool -v -cd nodemcu -cp $1 -cb 115200 -ca 0x00000000 -cf ../bin/$2/0x0-SSLAC16-latest.bin
